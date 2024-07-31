0868740a162cf23d15733fd4ac35c96505f89896
gamer <gamer@RUINOXD>
2024-07-31 20:02:34 +0300

    Add project files.

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultBrandComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultBrandComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+            { return View(); }
+    }
+}
